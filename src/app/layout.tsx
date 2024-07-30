346dbd2330f7ec8ade909f2188c551eb9aa2a73c
hhnguyen@hugeinc.com <hhnguyen@hugeinc.com>
2024-07-30 11:21:21 +0700

    Add image type

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,56 +18,66 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.ico" type="image/png" sizes="any" />
 
         <link
           rel="apple-touch-icon"
+          type="image/png"
           sizes="any"
           href={`/apple-touch-icon-57x57.png`}
         />
 
         <link
           rel="apple-touch-icon"
+          type="image/png"
           sizes="57x57"
           href={`/apple-touch-icon-57x57.png`}
         />
         <link
           rel="apple-touch-icon"
+          type="image/png"
           sizes="60x60"
           href={`/apple-touch-icon-60x60.png`}
         />
         <link
           rel="apple-touch-icon"
+          type="image/png"
           sizes="72x72"
           href={`/apple-touch-icon-72x72.png`}
         />
         <link
           rel="apple-touch-icon"
+          type="image/png"
           sizes="76x76"
           href={`/apple-touch-icon-76x76.png`}
         />
         <link
           rel="apple-touch-icon"
+          type="image/png"
           sizes="114x114"
           href={`/apple-touch-icon-114x114.png`}
         />
         <link
           rel="apple-touch-icon"
+          type="image/png"
           sizes="120x120"
           href={`/apple-touch-icon-120x120.png`}
         />
         <link
           rel="apple-touch-icon"
+          type="image/png"
           sizes="144x144"
           href={`/apple-touch-icon-144x144.png`}
         />
         <link
           rel="apple-touch-icon"
+          type="image/png"
           sizes="152x152"
           href={`/apple-touch-icon-152x152.png`}
         />
         <link
           rel="apple-touch-icon"
+          type="image/png"
           sizes="180x180"
           href={`/apple-touch-icon-180x180.png`}
         />
